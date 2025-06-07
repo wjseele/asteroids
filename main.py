e5056af05db0ac6457a921af46900a02a1045bec
wjseele <wjseele@pm.me>
2025-06-07 17:24:29 +0200

    Limited FPS

--- main.py
+++ main.py
@@ -13,9 +13,12 @@ def main():
         pygame.init()
     except Exception as e:
         print(f"Error encountered: {e}")
+
+    clock = pygame.time.Clock
+    dt = 0
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -23,6 +26,8 @@ def main():
         
         screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
