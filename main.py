3b1c2346d389c45534788cd28f8170a018048805
wjseele <wjseele@pm.me>
2025-06-07 17:20:29 +0200

    Potential screen

--- main.py
+++ main.py
@@ -9,5 +9,20 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    try:
+        pygame.init()
+    except Exception as e:
+        print(f"Error encountered: {e}")
+    
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        screen.fill("black")
+        pygame.display.flip()
+
 if __name__ == "__main__":
     main()
